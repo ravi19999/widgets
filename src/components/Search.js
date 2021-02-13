3e8b412bb47ccc64d88701615eb6e75258ea8208
Ravindra Dahal <dahalravindra@gmail.com>
2021-02-13 22:04:00 +0545

    Made requests to the wikipedia API and rendered the results onto the screen :sparkles:

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,54 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const Search = () => {
+	const [term,setTerm] = useState('cricket');
+	const [results, setResults] = useState([]);
+
+	console.log(results)
+
+	useEffect(() => {
+		(async() => {
+					const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+						params: {
+							action: 'query',
+							list: 'search',
+							origin: '*',
+							format: 'json',
+							srsearch: term
+						}
+					});
+					setResults(data.query.search)
+		})();
+	},[term]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className='item'>
+				<div className='content'>
+					<div className='header'>
+						{result.title}
+					</div>
+					{result.snippet}
+				</div>
+			</div>
+			)
+	})
 	return(
-		<>
-		<input type='text' placeholder='Search Here!' name='Search'/>
-		<label for='Search'>Search </label>
-		</>
+		<div>
+			<div className='ui form'>
+				<div className='field'>
+					<label>Enter Search Term</label>
+					<input 
+					value={term}
+					onChange={e => setTerm(e.target.value)}
+					className='input'
+					 />
+				</div>
+			</div>
+			<div className="ui celled list">
+				{renderedResults}
+			</div>
+		</div>
 	)
 }
 

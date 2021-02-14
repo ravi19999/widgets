14f7f54fce7cba9b565560f487c445ec26449e9c
Ravindra Dahal <dahalravindra@gmail.com>
2021-02-14 21:14:18 +0545

    created the dropdown component :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search'
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+
+const options = [
+    {
+        label: "The Color Red",
+        value: "red",
+    },
+    {
+        label: "The Color Green",
+        value: "green",
+    },
+    {
+        label: "A shade of Blue",
+        value: "blue",
+    },
+];
 const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
+    {
+        title: "What is React?",
+        content: "React is a front end javascript framework",
+    },
+    {
+        title: "Why use React?",
+        content: "React is a favorite JS library among engineers",
+    },
+    {
+        title: "How do you use React?",
+        content: "You use React by creating components",
+    },
 ];
 
-const App =  () => {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Dropdown options={options} />
+        </div>
+    );
 };
 
 export default App;
